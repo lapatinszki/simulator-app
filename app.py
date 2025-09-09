afa2ce14ed727b234a2f7b305603e34ffb66f19a
lapatinszki <lapatinszki18@gmail.com>
2025-09-09 12:37:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -60,11 +60,11 @@ if not st.session_state.logged_in:
                     st.session_state.nickname = nickname
                     
                     #E-mail küldése bejenlentkezésről! -- Csak guthubos deploy esetén menjen ki az e-mail
-                    if "STREMLIT_RUNTIME" in os.environ:
-                        app_email.send_email(email, st.session_state.email_hash, nickname)
-                    else:
-                        print("Not sending e-mail in local run.")
-                    email = "" #RESET AZONNAL!
+                    #if "STREMLIT_RUNTIME" in os.environ:
+                    app_email.send_email(email, st.session_state.email_hash, nickname)
+                    #else:
+                       # print("Not sending e-mail in local run.")
+                    #email = "" #RESET AZONNAL!
 
                     st.session_state.show_game_intro = True
                     st.rerun()
@@ -249,4 +249,5 @@ else:
                     st.rerun()
                     
                 st.warning("⚠️ Once you finish the game, you cannot return to attempts!")
+
 
