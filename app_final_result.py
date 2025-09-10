5df581ed86ee989a8935dbc54f71cd7a54bf97ef
lapatinszki <lapatinszki18@gmail.com>
2025-09-10 13:55:27 +0200

    Update app_final_result.py

--- app_final_result.py
+++ app_final_result.py
@@ -12,7 +12,7 @@ def calculate_results(github_token):
         if github_token == None: #Lokális futtatás
             rank = app_modify_tables.get_rank_for_profit(max_profit)
         else: #Cloud futtatás
-            rank = app_modify_GitTable.get_rank_for_profit(max_profit, "lapatinszki/simulator-app")
+            rank = app_modify_GitTable.get_rank_for_profit(max_profit, github_token, "lapatinszki/simulator-app")
 
         # --- Ordinal suffix függvény ---
         def ordinal(n):
