9eaefd0b445bd5d67e964168a1ec143277d23d34
lapatinszki <lapatinszki18@gmail.com>
2025-09-10 11:44:10 +0200

    Update app_display_results.py

--- app_display_results.py
+++ app_display_results.py
@@ -64,7 +64,7 @@ def play_the_GIF():
         height: 100%;
         top: 0;
         left: 0;
-        background-color: rgba(0, 0, 0, 0.85);
+        background-color: #0C172D;
         display: flex;
         justify-content: center;
         align-items: center;
