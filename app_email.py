286e341752315819b74383d1add3fc0031ae4622
lapatinszki <lapatinszki18@gmail.com>
2025-09-12 11:04:16 +0200

    Update app_email.py

--- app_email.py
+++ app_email.py
@@ -39,7 +39,30 @@ def send_results(receiver_email, nickname, profit):
     msg['To'] = receiver_email
     msg['Subject'] = "🏆 Factory Manager Challenge – Your results are in!"
 
-    body = "Teszt"
+    body = f"""
+     <html>
+     <body>
+         Dear <b>{nickname}</b>,<br><br>
+
+         Congratulations, you’ve completed the Mini Factory Challenge! 🏭<br>
+         Your best result: <b>€{profit}</b> out of the maximum possible <b>€619.78</b>.<br>
+         Not bad! 😎<br>
+         But think about it: in real life, every decision has an even bigger impact on costs, quality, sustainability, and profit. This game was just a glimpse into how many parameters, trade-offs, and choices shape modern production processes.<br><br>
+
+         <b>Relax, it’s just a game. Or… is it?</b> 👀<br><br>
+
+         Because in real manufacturing, the same trade-offs decide whether you’re making money, wasting energy, or just stockpiling reject parts.<br>
+         That’s where we come in: helping you find the sweet spot before the “Game Over” screen shows up in real life.<br><br>
+
+         <b>Use simulation to untap the hidden potential of your manufacturing today – and profit tomorrow!</b><br><br>
+
+         👉 Learn more: <a href="https://www.idm-systems.hu">www.idm-systems.hu</a><br><br>
+
+         Best regards,<br>
+         The IDM - Team
+     </body>
+     </html>
+     """
     msg.attach(MIMEText(body, 'plain'))
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -52,32 +75,8 @@ def send_results(receiver_email, nickname, profit):
     server.quit()
 
 
-    #msg['Subject'] = Header("🏆 Factory Manager Challenge – Your results are in!", 'utf-8')
 
-    # body = f"""
-    # <html>
-    # <body>
-    #     Dear <b>{nickname}</b>,<br><br>
-
-    #     Congratulations, you’ve completed the Mini Factory Challenge! 🏭<br>
-    #     Your best result: <b>€{profit}</b> out of the maximum possible <b>€619.78</b>.<br>
-    #     Not bad! 😎<br>
-    #     But think about it: in real life, every decision has an even bigger impact on costs, quality, sustainability, and profit. This game was just a glimpse into how many parameters, trade-offs, and choices shape modern production processes.<br><br>
-
-    #     <b>Relax, it’s just a game. Or… is it?</b> 👀<br><br>
-
-    #     Because in real manufacturing, the same trade-offs decide whether you’re making money, wasting energy, or just stockpiling reject parts.<br>
-    #     That’s where we come in: helping you find the sweet spot before the “Game Over” screen shows up in real life.<br><br>
-
-    #     <b>Use simulation to untap the hidden potential of your manufacturing today – and profit tomorrow!</b><br><br>
-
-    #     👉 Learn more: <a href="https://www.idm-systems.hu">www.idm-systems.hu</a><br><br>
-
-    #     Best regards,<br>
-    #     The IDM - Team
-    # </body>
-    # </html>
-    # """
+     
 
 
 #Checkolja az email címet:
