6d9693cea68dcde157473babf5ff07f8e889b842
lapatinszki <lapatinszki18@gmail.com>
2025-09-12 14:00:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ if st.session_state.scroll_to_top:
     scroll_to_here(0, key="top")  # Scroll to the top of the page
     st.session_state.scroll_to_top = False  # Reset the state after scrolling
 
-if st.session_state.scroll_to_top:
+if st.session_state.scroll_to_top_Delay:
     scroll_to_here(0, key="top")  # Scroll to the top of the page
     st.session_state.scroll_to_top_Delay = False  # Reset the state after scrolling
     time.sleep(0.7)
@@ -356,6 +356,7 @@ else:
                         st.session_state.confirm_finish = False
                         st.rerun()
             
+
 
 
 
