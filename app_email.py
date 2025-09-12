5c7668698f90a06efafbd37c9b1c7a2830a58659
lapatinszki <lapatinszki18@gmail.com>
2025-09-12 08:12:31 +0200

    Update app_email.py

--- app_email.py
+++ app_email.py
@@ -1,10 +1,13 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import re
+import dns.resolver
 import os
 
+# Küldés saját Gmailre - bejelentkezés logolása:
 def send_email(email, email_hash, nickname):
-    # Küldés saját Gmailre
+    
     sender_email = os.environ["GMAIL_EMAIL"]
     app_password = os.environ["GMAIL_APP_PASSWORD"]
     receiver_email = sender_email  # saját magadnak küldjük
@@ -14,7 +17,7 @@ def send_email(email, email_hash, nickname):
     msg['To'] = receiver_email
     msg['Subject'] = f"Új belépés: {nickname}"
 
-    body = f"Felhasználó email: {email}\nEmail hash kód: {email_hash}\nNickname: {nickname}"
+    body = f"Email: {email}\nEmail hash kód: {email_hash}\nNickname: {nickname}"
     msg.attach(MIMEText(body, 'plain'))
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -23,3 +26,42 @@ def send_email(email, email_hash, nickname):
     server.sendmail(sender_email, receiver_email, msg.as_string())
     server.quit()
 
+
+
+#Eredmény elküldése felhasználó e-mailre:
+def send_results(receiver_email, nickname):
+    sender_email = os.environ["GMAIL_EMAIL"]
+    app_password = os.environ["GMAIL_APP_PASSWORD"]
+
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
+    msg['Subject'] = f"Új belépés: {nickname}"
+    body = ""
+
+
+    msg.attach(MIMEText(body, 'plain'))
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(sender_email, app_password)
+    server.sendmail(sender_email, receiver_email, msg.as_string())
+    server.quit()
+
+
+
+
+#Checkolja az email címet:
+def is_valid_email(email: str) -> bool:
+    # 1. Regex: alap szintaktikai ellenőrzés
+    regex = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+    if not re.match(regex, email):
+        return False
+    
+    # 2. MX rekord ellenőrzés
+    try:
+        domain = email.split("@")[1]
+        answers = dns.resolver.resolve(domain, "MX")
+        return len(answers) > 0
+    except Exception:
+        return False
