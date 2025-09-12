658e89302392cf3b9cd8661db2c574ea9d72fb7b
lapatinszki <lapatinszki18@gmail.com>
2025-09-12 13:52:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ if 'scroll_to_header' not in st.session_state:
 if st.session_state.scroll_to_top:
     scroll_to_here(0, key="top")  # Scroll to the top of the page
     st.session_state.scroll_to_top = False  # Reset the state after scrolling
+    time.sleep(1)
     st.rerun()    
 
 def scroll():
@@ -347,6 +348,7 @@ else:
                         st.session_state.confirm_finish = False
                         st.rerun()
             
+
 
 
 
